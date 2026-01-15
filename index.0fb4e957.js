a718f11a715c7ed740554809b0c02842655ed0aa
shymkivvasyl <shymkiv.vasyl333@gmail.com>
2026-01-15 16:21:54 +0100

    make build

---
+++ index.0fb4e957.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table").rows,r=!0,l=!1,t=void 0;try{for(var o,n=e[Symbol.iterator]();!(r=(o=n.next()).done);r=!0){var a=o.value,c=a.cells,i=c[1].cloneNode(!0);a.insertBefore(i,c[c.length-1])}}catch(e){l=!0,t=e}finally{try{r||null==n.return||n.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.0fb4e957.js.map
